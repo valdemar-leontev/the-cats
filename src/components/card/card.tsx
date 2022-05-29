00607c44707c3595ddbbab304f0b31c3af893c90
Vladimir <valdemar.leontev@gmail.com>
2022-05-29 16:52:38 +0300

    Bag fix

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -21,7 +21,7 @@ const Card = ({imageUrl} : CardProps) => {
             <img src={imageUrl} alt={`${imageUrl}`} />
 
             <div className="app__card-heart" onClick={onHeartClick}>
-              {active == false ? <AiOutlineHeart size={30} color={'red'}/> : <AiFillHeart size={30} color={'red'}/>}
+              {active === false ? <AiOutlineHeart size={30} color={'red'}/> : <AiFillHeart size={30} color={'red'}/>}
             </div>
         </div>
     </>
